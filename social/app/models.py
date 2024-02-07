3e0600d8f7c3b130d41317bf93f86ebfd5ae311c
limoo <limoodrice@gmail.com>
2024-02-07 09:34:33 +0300

    feat: created a Tag model

--- social/app/models.py
+++ social/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from autoslug import AutoSlugField
 
 # Create your models here.
 class Post(models.Model):
@@ -8,6 +9,7 @@ class Post(models.Model):
     artist = models.CharField(max_length=500, null=True)
     url = models.URLField(max_length=500, null=True)
     body = models.TextField()
+    tags = models.ManyToManyField('Tag')
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(max_length=100,default=uuid.uuid4,unique=True,primary_key=True,editable=False)
     
@@ -17,3 +19,12 @@ class Post(models.Model):
     class Meta:
         db_table = "posts"
         ordering = ['-created']
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.FileField(upload_to='icons/',null=True,blank=True)
+    slug = AutoSlugField(populate_from='name',unique=True)
+    
+    def __str__(self):
+        return str(self.name)
+    
