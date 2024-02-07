40334aea639458112ae4536feea1a76d393351bc
limoo <limoodrice@gmail.com>
2024-02-07 09:42:07 +0300

    feat: registered category view url

--- social/app/urls.py
+++ social/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import home_page, create_post, delete_post, edit_post, read_post
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',home_page,name='home'),
@@ -7,4 +9,7 @@ urlpatterns = [
     path('post/delete/<pk>/',delete_post,name='post-delete'),
     path('post/edit/<pk>/',edit_post,name='post-edit'),
     path('post/read/<pk>/',read_post,name='post-read'),
+    path('post/category/<tag>/',home_page,name='post-category'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
